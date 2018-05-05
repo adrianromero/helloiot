56cf199d721ebe89dd9ab7428161eb060798b695
Adrián Romero <adrianromero@users.noreply.github.com>
2018-05-05 17:12:41 +0200

    Improved fxThread executor

--- src/main/java/com/adr/helloiot/util/CompletableAsync.java
+++ src/main/java/com/adr/helloiot/util/CompletableAsync.java
@@ -1,5 +1,5 @@
 //    HelloIoT is a dashboard creator for MQTT
-//    Copyright (C) 2017 Adrián Romero Corchado.
+//    Copyright (C) 2017-2018 Adrián Romero Corchado.
 //
 //    This file is part of HelloIot.
 //
@@ -42,9 +42,13 @@ public class CompletableAsync<T> {
         return new Executor() {
             @Override
             public void execute(Runnable command) {
-                Platform.runLater(() -> {
+                if (Platform.isFxApplicationThread()) {
                     command.run();
-                });          
+                } else {
+                    Platform.runLater(() -> {
+                        command.run();
+                    });        
+                }
             }
         };
     }
