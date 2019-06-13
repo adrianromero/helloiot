260b6a474cd4724a0ddd72a7a6b8fc33d07a0a28
Adrián Romero <adrianromero@users.noreply.github.com>
2019-06-14 00:16:16 +0200

    Fancy loading animation

--- src/main/java/com/adr/helloiot/util/Dialogs.java
+++ src/main/java/com/adr/helloiot/util/Dialogs.java
@@ -19,25 +19,72 @@
 package com.adr.helloiot.util;
 
 import com.adr.hellocommon.dialog.DialogView;
-import javafx.scene.control.ProgressBar;
+import javafx.animation.RotateTransition;
+import javafx.scene.CacheHint;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeType;
+import javafx.util.Duration;
 
-/**
- *
- * @author adrian
- */
 public class Dialogs {
     
     private Dialogs() {}
 
     public static DialogView createLoading() {
-        ProgressBar p = new ProgressBar();    
-        p.getStyleClass().add("loading-bar");
-
         DialogView dialog = new DialogView();
         dialog.setCSS("/com/adr/helloiot/styles/loading.css");
         dialog.setMaster(true);
         dialog.setAnimate(false);
-        dialog.setContent(p);
+        dialog.setContent(createLoadingNode ());
         return dialog;      
+    }
+    
+    private static Node createLoadingNode () {
+        
+        Circle c0 = new Circle(65);
+        c0.setFill(Color.TRANSPARENT);
+        c0.setStrokeWidth(0.0);
+
+        Circle c1 = new Circle(50);
+        c1.setFill(Color.TRANSPARENT);
+        c1.setStrokeType(StrokeType.INSIDE);
+        c1.setStrokeLineCap(StrokeLineCap.BUTT);
+        c1.getStrokeDashArray().addAll(78.54); // 50 * 2 * 3.1416 / 4
+        c1.setCache(true);
+        c1.setCacheHint(CacheHint.ROTATE);   
+        c1.getStyleClass().add("loading-circle");
+        setRotate(c1, true, 440.0, 10);
+
+        Circle c2 = new Circle(40);
+        c2.setFill(Color.TRANSPARENT);
+        c2.setStrokeType(StrokeType.INSIDE);
+        c2.setStrokeLineCap(StrokeLineCap.BUTT);
+        c2.getStrokeDashArray().addAll(41.89); // 40 * 2 * 3.1416 / 6
+        c2.setCache(true);
+        c2.setCacheHint(CacheHint.ROTATE);    
+        c2.getStyleClass().add("loading-circle");
+        setRotate(c2, true, 360.0, 14);
+
+        Circle c3 = new Circle(30);
+        c3.setFill(Color.TRANSPARENT);
+        c3.setStrokeType(StrokeType.INSIDE);
+        c3.setStrokeLineCap(StrokeLineCap.BUTT);        
+        c3.getStyleClass().add("loading-circle");
+
+        return new Group(c0, c1, c2, c3);
+    }
+    
+    private static void setRotate(Shape s, boolean reverse, double angle, int duration) {
+        RotateTransition r = new RotateTransition(Duration.seconds(duration), s);
+        r.setAutoReverse(reverse);
+        r.setDelay(Duration.ZERO);
+        r.setRate(3.0);
+        r.setCycleCount(RotateTransition.INDEFINITE);
+        r.setByAngle(angle);
+        r.play();
     }
 }
